d60e1b8f5d2ece98f9a9056008438c9092a707c5
ArpitChugh <ArpitChugh.jbd.ac@gmail.com>
2021-10-17 17:34:09 +0530

    task 2 completed

--- submissions/1902017/random-ecard-generator/src/App.js
+++ submissions/1902017/random-ecard-generator/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [details, setDetails] = useState({});
+	useEffect(() => {
+		axios.get('https://randomuser.me/api/').then((response) => {
+			setDetails(response.data.results[0]);
+			console.log(response);
+		});
+	}, []);
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className="blurred-box">
+					<div className='content'>
+						<p>
+							<img
+								className="profile-pic"
+								src={details.picture?.large}
+								alt="profile pic"
+							/>
+						</p>
+
+						<h1>
+							{details.name?.title} {details.name?.first}
+							{details.name?.last}
+						</h1>
+						<h4>
+							Email: <span> {details.email}</span>
+						</h4>
+						<h4>
+							Phone: <span> {details.phone}</span>
+						</h4>
+						<p>
+							Country: <span> {details.location?.country}</span>
+						</p>
+						<p>
+							State: <span> {details.location?.state}</span>
+						</p>
+						<p>
+							City: <span> {details.location?.city}</span>
+						</p>
+					</div>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
